aab1f739a9c13da71907d81669792ff9a86e77cc
Soujanya Mallick <soujanyamallick199@gmail.com>
2025-07-15 11:34:54 +0530

    Added User model and Message schema

---
+++ src/model/User.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Message extends Document {
+    content: string;
+    createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+    content: { type: String, required: true },
+    createdAt: { type: Date, required: true, default: Date.now }
+});
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpry: Date;
+    isVerified: boolean
+    isAcceptingMsg: boolean;
+    messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, match: [/.+\@.+\..+/, 'Please use a valid email address.'] },
+    password: { type: String, required: true },
+    verifyCode: { type: String, required: true },
+    verifyCodeExpry: { type: Date, required: true },
+    isVerified: { type: Boolean, default: false },
+    isAcceptingMsg: { type: Boolean, default: true },
+    messages: { type: [MessageSchema], default: [] }
+});
+
+const UserModel = (mongoose.models.user as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema);
+
+export default UserModel;
